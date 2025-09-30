b0a79f8aee9790bc314942efae01bdb59942b203
Danilo Urrego <daniloud2809@gmail.com>
2025-09-30 10:02:20 -0500

    Feature: Comentar limitadores de request

--- routes/auth.js
+++ routes/auth.js
@@ -5,28 +5,30 @@ import checkAuth from '../middleware/checkAuth.js';
 import rateLimit from 'express-rate-limit';
 const router = express.Router();
 
-const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutos
-  max: 5, // Solo 5 intentos
-  message: 'Demasiados intentos de login. Intenta en 15 minutos',
-  skipSuccessfulRequests: true, // No cuenta los login exitosos
-});
+// Limitadores de tasa para proteger contra ataques de fuerza bruta
 
-const passwordRecoveryLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 1 hora
-  max: 3, // Solo 3 intentos de recuperación
-  message: 'Demasiadas solicitudes de recuperación. Intenta en 1 hora',
-});
+// const loginLimiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutos
+//   max: 5, // Solo 5 intentos
+//   message: 'Demasiados intentos de login. Intenta en 15 minutos',
+//   skipSuccessfulRequests: true, // No cuenta los login exitosos
+// });
+
+// const passwordRecoveryLimiter = rateLimit({
+//   windowMs: 60 * 60 * 1000, // 1 hora
+//   max: 3, // Solo 3 intentos de recuperación
+//   message: 'Demasiadas solicitudes de recuperación. Intenta en 1 hora',
+// });
 
 // login y registro
 router.post('/registrar', validarRegistro, registrar);
-router.post('/autenticar', loginLimiter, validarLogin, autenticar);
+router.post('/autenticar', validarLogin, autenticar);
 
 // Confirmar cuenta
 router.get('/confirmar/:token', confirmar);
 
 // Contraseña
-router.post('/recuperar-password', passwordRecoveryLimiter, validarEmail, recuperarPassword);
+router.post('/recuperar-password', validarEmail, recuperarPassword);
 router.get('/recuperar-password/:token', comprobarToken);
 router.post('/recuperar-password/:token', validarNuevaPassword, nuevaPassword);
 
